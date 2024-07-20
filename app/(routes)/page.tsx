e13d6b9bf098b80efc8e8ae889f544da84beb4f0
ldjimenezt <ldjimenezt@utn.edu.ec>
2024-07-19 23:08:50 -0500

    fix: build

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -7,7 +7,7 @@ import { SalesDistributors } from "./components/SalesDistributor";
 import { TotalSuscribers } from "./components/TotalSuscribers";
 import { ListIntegrations } from "./components/ListIntegrations";
 
-export const dataCardsSummary = [
+const dataCardsSummary = [
   {
     icon: UsersRound,
     total: "12.450",
